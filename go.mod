23444d229b46757fd11a52bb0409f8d4c93cb41d
AlexanderAST <89164909109@bk.ru>
2022-10-14 20:10:09 +0300

    added authentication,jwt tokens,parse JWT tokens

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module todo-app
 go 1.19
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.8.1
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.2.0
+	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.13.0
 )
 
--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -21,6 +21,25 @@ func (h *Handler) singUp(c *gin.Context) {
 		"id": id,
 	})
 }
+
+type SignInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func (h *Handler) singIn(c *gin.Context) {
+	var input SignInInput
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 
 }
